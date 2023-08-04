3e525952cea17851c2d469a600f0037d29e5835a
Devansh Batham <devanshbatham009@gmail.com>
2023-08-04 18:09:39 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Rename the ip2cloud.py file to watson
+mv ip2cloud.py ip2cloud
+
+# Move the ip2cloud file to /usr/local/bin
+sudo mv ip2cloud /usr/local/bin/
+
+# Make the ip2cloud file executable
+sudo chmod +x /usr/local/bin/ip2cloud
+
+# Remove the ip2cloud.pyc file if it exists
+if [ -f ip2cloud.pyc ]; then
+    rm ip2cloud.pyc
+fi
+
+echo "ip2cloud has been installed successfully!"
